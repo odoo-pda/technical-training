13eef2e60a640ccddc9a44c8edb6eb6d2007ad56
odoo-pda <38421305+odoo-pda@users.noreply.github.com>
2019-06-06 09:12:28 +0000

    [OA] constrains on nb of attendees

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import Warning
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -29,3 +32,27 @@ class Session(models.Model):
     instructor_id = fields.Many2one('openacademy.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('openacademy.partner', string="Attendees")
+
+    max_attendees = fields.Integer(string="Max")
+    nb_attendees = fields.Integer(string="Number of Attendees", compute='_compute_attendees', store=True)
+    
+    @api.depends('attendee_ids')
+    def _compute_attendees(self):
+        self.nb_attendees = len(self.attendee_ids)
+        
+    """@api.constrains('max_attendees','attendee_ids')
+    def check_nb_attendees(self):
+        _logger.info("CONSTRAINS-------------------NB ATT BEFORE %s-------" % self.nb_attendees)
+        if self.nb_attendees > self.max_attendees:
+            #return {'warning': {
+            #    'title': 'Too many attendees',
+            #    'message': 'The room has available seats and there is attendees registered'
+            #}}
+            raise Warning("Too much attendees for this session.")
+        _logger.info("CONSTRAINS-------------------NB ATT AFTER %s-------" % self.nb_attendees)
+    """
+        
+    _sql_constraints = [
+        # possible only if taken_seats is stored
+        ('session_full', 'CHECK(nb_attendees <= max_attendees)', 'The room is full'),
+    ]
